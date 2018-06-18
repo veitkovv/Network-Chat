d3cd1f89b99d95a97cd12d4a3469cc172be31aff
veytko-vv <veytko-vv@yamal-region.tv>
2018-06-18 17:52:15 +0500

    simplify chats request

--- client/ui/console_ui.py
+++ client/ui/console_ui.py
@@ -58,7 +58,7 @@ class ConsoleClient(BaseUI):
         print('+name - add contact')
         print('-name - delete contact')
         print('/list - contact list')
-        print('/chatlist - list of chats on the server')
+        print('/chats - list of chats on the server')
         print('/join chat_name - join the chat_name')
         print('/leave chat_name - leave chat_name')
         print('? - display this message')
@@ -71,7 +71,7 @@ class ConsoleClient(BaseUI):
         log.debug(f'processing {message}')
         response_obj = Response(message)
         if not response_obj.action == 'msg':
-            print(f'SERVER MESSAGE:<< <{self.timestamp_to_normal_date(response_obj.headers["time"])}> '
+            print(f'<{self.timestamp_to_normal_date(response_obj.headers["time"])}> SERVER MESSAGE: '
                   f'<ACTION: {response_obj.action}> '
                   f'<CODE: {response_obj.code}> : {response_obj.body}')
 
@@ -101,7 +101,7 @@ class ConsoleClient(BaseUI):
             return Request(action='del_contact', body=new_contact)
         elif user_input_message.startswith('/list'):
             return Request(action='get_contacts', body='')
-        elif user_input_message.startswith('/chatlist'):
+        elif user_input_message.startswith('/chats'):
             return Request(action='get_chats', body='')
         elif user_input_message.startswith('/join'):
             try:
